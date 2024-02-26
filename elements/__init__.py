f404d21055a38f2a024dbbb81e6de20c42ba3322
Danijar Hafner <mail@danijar.com>
2024-02-26 02:05:39 +0000

    Add timestamp utility.

--- elements/__init__.py
+++ elements/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '3.1.2'
+__version__ = '3.2.0'
 
 from .agg import Agg
 from .checkpoint import Checkpoint
@@ -14,6 +14,7 @@ from .rwlock import RWLock
 from .timer import Timer
 from .usage import Usage
 from .uuid import UUID
+from .utils import timestamp
 
 from . import logger
 from . import plotting
