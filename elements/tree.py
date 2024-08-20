02524b4a44d0ec58ee508bbeeb3e05564e091ac7
Danijar Hafner <mail@danijar.com>
2024-08-20 19:14:19 +0000

    Clean up nestdict assert

--- elements/tree.py
+++ elements/tree.py
@@ -69,8 +69,8 @@ def nestdict(mapping, sep='/'):
     if isinstance(tree, dict):
       tree = {k: post(v) for k, v in tree.items()}
       if all(k.startswith('[') and k.endswith(']') for k in tree):
-        assert set([int(x[1:-1]) for x in tree.keys()]) == set(range(len(tree))), (
-            sorted(tree.keys()))
+        available = set(int(x[1:-1]) for x in tree.keys())
+        assert available == set(range(len(tree))), available
         tree = tuple(tree[f'[{i}]'] for i in range(len(tree)))
     return tree
   tree = post(tree)
